ffc0622684180a2dcc82fc2333d3811911dc426c
Dmitry Uchkin <not3hunnit@icloud.com>
2023-09-13 00:20:12 +0300

    Done

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -14,7 +14,7 @@ function EditProfilePopup(props) {
     React.useEffect(() => {
         setName(currentUser.name);
         setDescription(currentUser.about);
-    }, [currentUser]);
+    }, [currentUser, props.isOpen]);
 
     function handleChangeName(evt) {
         setName(evt.target.value);
